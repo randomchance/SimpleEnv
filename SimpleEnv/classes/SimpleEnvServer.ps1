76445411b5e7a6525afd670b29f021e16ec3359f
Chance Carroll <chancecarroll@live.com>
2020-01-21 12:56:44 -0500

    Update SimpleEnvServer.ps1
    Added tag and role helper methods for #1

--- SimpleEnv/classes/SimpleEnvServer.ps1
+++ SimpleEnv/classes/SimpleEnvServer.ps1
@@ -7,4 +7,23 @@ class SimpleEnvServer
     [string[]] $Roles = @()
     [string[]] $Tags = @()
     [hashtable] $Properties = @{ }
+    
+    [void] AddTag([string]$Tag)
+    {
+        $this.Tags = $this.Tags + $Tag | Sort-Object -Unique
+    }
+    [void] RemoveTag([string]$Tag)
+    {
+        $this.Tags = $this.Tags | Where-Object { $_ -notlike $Tag }
+    }
+
+    [void] AddRole([string]$Role)
+    {
+        $this.Roles = $this.Roles + $Role | Sort-Object -Unique
+    }
+    [void] RemoveRole([string]$Role)
+    {
+        $this.Roles = $this.Roles | Where-Object { $_ -notlike $Role }
+    }
+    
 }
